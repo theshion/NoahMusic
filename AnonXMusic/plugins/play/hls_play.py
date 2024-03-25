41352a6c2ffe26c3b1ef372055c702ebc7382098
Naitik Sharma <147588654+theshion@users.noreply.github.com>
2024-03-25 19:55:06 +0530

    Create hls_play.py

---
+++ AnonXMusic/plugins/play/hls_play.py
@@ -0,0 +1,44 @@
+import asyncio
+import os
+
+from pyrogram import Client, filters
+from pytgcalls import GroupCallFactory
+from pytgcalls.exceptions import GroupCallNotFoundError
+
+
+CHAT_ID = -100123456789  # Apne group ka unique ID yahan update karein
+PLAY_URL = "https://prod-ent-live-gm.jiocinema.com/hls/live/2100323/hd_akamai_androidmob_avc_hin_ipl_s1_m1250324/master_720p.m3u8"
+
+
+app = Client("my_bot")
+
+
+group_call_factory = GroupCallFactory(app, GroupCallFactory.MTPROTO_CLIENT_TYPE.PYROGRAM)
+
+
+@app.on_message(filters.command("play") & filters.chat(CHAT_ID))
+async def play(_, message):
+    group_call = group_call_factory.get_group_call()
+    try:
+        await group_call.join(CHAT_ID)
+    except GroupCallNotFoundError:
+        await message.reply_text("You must first start a voice chat in this group!")
+    else:
+        await group_call.start_audio(PLAY_URL)
+        await message.reply_text("Playing...")
+
+
+@app.on_message(filters.command("stop") & filters.chat(CHAT_ID))
+async def stop(_, message):
+    group_call = group_call_factory.get_group_call()
+    await group_call.stop_audio()
+    await message.reply_text("Stopped!")
+
+
+async def main():
+    await app.start()
+    await idle()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
